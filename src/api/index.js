05c5c3a9be033ba1d0d88b6e6cae393413aaa112
ahsefati <ahsefati77@gmail.com>
2024-05-27 21:37:12 -0600

    Changed api base url.

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// const API = axios.create({baseURL: 'http://35.93.98.239:8080/'})
-const API = axios.create({baseURL: 'http://localhost:5000/'})
+const API = axios.create({baseURL: 'http://35.93.98.239:8080/'})
+// const API = axios.create({baseURL: 'http://localhost:5000/'})
 
 API.interceptors.request.use((req)=>{
     if (localStorage.getItem('profile')){
